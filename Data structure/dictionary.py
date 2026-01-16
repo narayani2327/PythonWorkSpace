74373750dbcb58c6b643f441b1f9e555248a1dff
unknown <harikalanarayani@gmail.com>
2026-01-16 10:42:09 +0530

    address

--- Data structure/dictionary.py
+++ Data structure/dictionary.py
@@ -26,3 +26,33 @@ print(my_friends.items())
 
 del my_friends["John"]      # Another way to delete a specific key
 print(my_friends.items())
+
+information = {
+    "name": "Alice",
+    "age": 28,
+    "address": {
+        "home": {
+            "street": "123 Main St",
+            "city": "Wonderland",
+            "zip": "12345",
+        },
+        "work": {
+            "street": "456 Work Rd",
+            "city": "Worktown",
+            "zip": "67890",
+        },
+        "natives": {
+            "street": "456 Work Rd",
+            "city": "Worktown",
+            "zip": "67890",
+        },
+    },
+    "hobbies": ["reading", "traveling", "swimming"],
+}
+
+
+print(information["name"])
+print(information["address"]["home"]["city"])
+print(information["hobbies"][1])
+
+print(information["address"]["natives"]["zip"])
