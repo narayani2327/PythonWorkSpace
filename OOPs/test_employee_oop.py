e7b3e65ac8b2322903a5dcc961d999466e8e1816
unknown <harikalanarayani@gmail.com>
2026-01-19 11:48:12 +0530

    employee id checks

--- OOPs/test_employee_oop.py
+++ OOPs/test_employee_oop.py
@@ -12,15 +12,36 @@ class Employee:
 
     def display_details(self):
         """Display employee details."""
-        print(self.name)
-        print(self.employee_id)
-        print(self.salary)
+        print(f"Name: {self.name}")
+        print(f"Employee ID: {self.employee_id}")
+        print(f"Salary: {self.salary}")
 
 
 def main():
     """Create an Employee instance and print its details."""
-    employee = Employee("Narayani", 123, 1233)
-    employee.display_details()
+    employee1 = Employee("Narayani", 123, 1233)
+    employee3 = Employee("Hari", 78, 98)
+    employee2 = Employee("Kalavathi", 164, 9087)
+    employee1.display_details()
+    employees = [employee1, employee2, employee3]
+    for item in employees:
+        print("____Employee Details____")
+        item.display_details()
+    for i in range(1, 10):
+        print(i)
+    employee4 = Employee("Kavya", 164, 89937)
+    valid = True
+    for item in employees:
+        if item.employee_id == employee4.employee_id:
+            valid = False
+
+    if valid:
+        employees.append(employee4)
+    else:
+        print("Employee ID exists")
+    for item in employees:
+        print("____Employee Details____")
+        item.display_details()
 
 
 if __name__ == "__main__":
