e802804f68e7fe6746531ed2c09d7d04c5924449
unknown <harikalanarayani@gmail.com>
2026-01-19 10:46:24 +0530

    even or odd numbers

--- Conditional statements/testing.py
+++ Conditional statements/testing.py
@@ -1,8 +1,12 @@
 """even or odd"""
 
 
-num = int(input("Enter the number: "))
-if num % 2 == 0:
-    print("Number is even")
-else:
-    print("Number is odd")
+def even_or_odd(num):
+    if num % 2 == 0:
+        print("Number is even")
+    else:
+        print("Number is odd")
+
+
+number = int(input("Enter the number: "))
+even_or_odd(number)
